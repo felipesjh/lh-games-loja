314095c31bcc763c99663f68d3d5e1048a6d1bf4
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-06-29 14:27:09 -0300

    Criando componente restrito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { InicioComponent } from './inicio/inicio.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { RestritoComponent } from './restrito/restrito.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { RodapeComponent } from './rodape/rodape.component';
     InicioComponent,
     LoginComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestritoComponent
   ],
   imports: [
     BrowserModule,
