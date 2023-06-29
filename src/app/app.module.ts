3132b7c23241dd058c82fda765bb9b9b64e86e4a
DanielleMauro16 <eng.danielle.mauro16@gmail.com>
2023-06-29 14:36:56 -0300

    Componente inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,10 @@ import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { RestritoComponent } from './restrito/restrito.component';
+import { AtualizaProdutoComponent } from './restrito/atualiza-produto/atualiza-produto.component';
+import { CadastroProdutoComponent } from './restrito/cadastro-produto/cadastro-produto.component';
+import { ListaProdutoComponent } from './restrito/lista-produto/lista-produto.component';
+import { MenuRestritoComponent } from './restrito/menu-restrito/menu-restrito.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +31,11 @@ import { RestritoComponent } from './restrito/restrito.component';
     LoginComponent,
     MenuComponent,
     RodapeComponent,
-    RestritoComponent
+    RestritoComponent,
+    AtualizaProdutoComponent,
+    CadastroProdutoComponent,
+    ListaProdutoComponent,
+    MenuRestritoComponent
   ],
   imports: [
     BrowserModule,
